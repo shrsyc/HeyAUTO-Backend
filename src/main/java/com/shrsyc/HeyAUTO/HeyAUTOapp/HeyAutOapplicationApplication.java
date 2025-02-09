864879e98c60a1d7d73a137671dcc10c2d1dc38f
shrsyc <shrsyc@gmail.com>
2025-02-09 08:29:24 +0530

    create app file structure

---
+++ src/main/java/com/shrsyc/HeyAUTO/HeyAUTOapp/HeyAutOapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.shrsyc.HeyAUTO.HeyAUTOapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyAutOapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyAutOapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrsyc/HeyAUTO/HeyAUTOapp/HeyAutOapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrsyc.HeyAUTO.HeyAUTOapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyAutOapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
